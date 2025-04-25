d60e9a3cde05b3f0dbb4b6a6dfcf8aa5b4749888
janina280 <coceijanina@yahoo.com>
2025-04-25 13:55:10 +0300

    symetric

---
+++ week_1/Symetric/src/main/java/org/interview/Source.java
@@ -0,0 +1,34 @@
+package org.interview;
+
+/* Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).*/
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int isSymmetric(TreeNode A) {
+        if (A == null) return 1;
+        return isMirror(A.left, A.right) ? 1 : 0;
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+
+        return (t1.val == t2.val)
+                && isMirror(t1.left, t2.right)
+                && isMirror(t1.right, t2.left);
+    }
+}
+
+
